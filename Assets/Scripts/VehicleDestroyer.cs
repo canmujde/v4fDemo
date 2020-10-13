87f4db4adee6131ff8f3338e53c9a5c89e8f171d
Can <can.mujde@icloud.com>
2020-10-14 01:05:37 +0300

    optimization fixes

---
+++ Assets/Scripts/VehicleDestroyer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleDestroyer : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Vehicle"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
